cfa2505e21b5cc9b9fd4ee6f426340cc7f8fab1a
Artem Solomatin <artem00298@gmail.com>
2025-08-04 17:14:43 +0300

    Add styles and icons

--- lib/neuro_scav_web.ex
+++ lib/neuro_scav_web.ex
@@ -17,7 +17,9 @@ defmodule NeuroScavWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths,
+    do:
+      ~w(assets fonts images favicon.ico robots.txt site.webmanifest favicon-32x32.png favicon-16x16.png apple-touch-icon.png android-chrome-512x512.png android-chrome-192x192.png)
 
   def router do
     quote do
