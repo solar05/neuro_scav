14fe3494f1cf365d1f0e0b146b7a74ebeadcc131
Artem Solomatin <artem00298@gmail.com>
2025-08-06 02:46:49 +0300

    Update set locale logic

--- lib/neuro_scav_web/plugs/set_locale_plug.ex
+++ lib/neuro_scav_web/plugs/set_locale_plug.ex
@@ -5,23 +5,30 @@ defmodule NeuroScavWeb.Plugs.SetLocale do
 
   import Plug.Conn
   @supported_locales Gettext.known_locales(NeuroScavWeb.Gettext)
+  @default_locale "en"
 
   def init(_options), do: nil
 
   def call(conn, _options) do
     case fetch_locale_from(conn) do
       nil ->
-        conn
+        put_locale(conn, @default_locale)
 
       locale ->
         NeuroScavWeb.Gettext |> Gettext.put_locale(locale)
-        put_resp_cookie(conn, "lang", locale, max_age: 365 * 24 * 60 * 60)
+        put_locale(conn, locale)
     end
+  end
+
+  defp put_locale(conn, locale) do
+    conn
+    |> put_resp_cookie("lang", locale, max_age: 365 * 24 * 60 * 60)
+    |> put_session("lang", locale)
   end
 
   defp fetch_locale_from(conn) do
     (conn.params["lang"] || conn.cookies["lang"])
-    |> check_locale
+    |> check_locale()
   end
 
   defp check_locale(locale) when locale in @supported_locales, do: locale
