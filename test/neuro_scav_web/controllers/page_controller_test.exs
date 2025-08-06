851c92a1f85bae17c778a233167d191628b29307
Artem Solomatin <artem00298@gmail.com>
2025-08-06 04:47:54 +0300

    Fix formatting

--- test/neuro_scav_web/controllers/page_controller_test.exs
+++ test/neuro_scav_web/controllers/page_controller_test.exs
@@ -3,6 +3,8 @@ defmodule NeuroScavWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 302) =~ "<html><body>You are being <a href=\"/neuro_scavengers\">redirected</a>.</body></html>"
+
+    assert html_response(conn, 302) =~
+             "<html><body>You are being <a href=\"/neuro_scavengers\">redirected</a>.</body></html>"
   end
 end
