ae69ce39964ddcbd02202c17439c9961e9cb7754
Artem Solomatin <artem00298@gmail.com>
2025-08-06 04:42:19 +0300

    Disable test for a while

--- test/neuro_scav_web/controllers/page_controller_test.exs
+++ test/neuro_scav_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule NeuroScavWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 302) =~ "<html><body>You are being <a href=\"/neuro_scavengers\">redirected</a>.</body></html>"
   end
 end
