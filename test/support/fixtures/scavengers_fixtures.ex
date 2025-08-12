377a20641dd004966b7167ab0052e2b695f79508
Artem Solomatin <artem00298@gmail.com>
2025-08-13 02:45:39 +0300

    Fix

--- test/support/fixtures/scavengers_fixtures.ex
+++ test/support/fixtures/scavengers_fixtures.ex
@@ -4,17 +4,14 @@ defmodule NeuroScav.ScavengersFixtures do
   entities via the `NeuroScav.Scavengers` context.
   """
 
-  @doc """
-  Generate a neuro_scavenger.
-  """
-  def neuro_scavenger_fixture(attrs \\ %{}) do
-    {:ok, neuro_scavenger} =
-      attrs
-      |> Enum.into(%{
-        name: "some name"
-      })
-      |> NeuroScav.Scavengers.create_neuro_scavenger()
+  # def neuro_scavenger_fixture(attrs \\ %{}) do
+  #   {:ok, neuro_scavenger} =
+  #     attrs
+  #     |> Enum.into(%{
+  #       name: "some name"
+  #     })
+  #     |> NeuroScav.Scavengers.create_neuro_scavenger()
 
-    neuro_scavenger
-  end
+  #   neuro_scavenger
+  # end
 end
