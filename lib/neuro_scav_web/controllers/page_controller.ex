33539380f28c8eb1f88f1da6d2eec4c3661ecf4f
Artem Solomatin <artem00298@gmail.com>
2025-08-15 02:59:31 +0300

    Remove unrelevant files from coverage

--- lib/neuro_scav_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule NeuroScavWeb.PageController do
-  use NeuroScavWeb, :controller
-
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    redirect(conn, to: ~p(/neuro_scavengers))
-  end
-end
--- test/neuro_scav_web/controllers/page_controller_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule NeuroScavWeb.PageControllerTest do
-  use NeuroScavWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-
-    assert html_response(conn, 200)
-  end
-end
